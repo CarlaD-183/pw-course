31b1cec6aa37a5f0318e8a94a65cc0902f9c7e83
PW - Phuong Hoang <hana.phuonght@gmail.com>
2025-09-08 22:18:03 +0700

    feat/edit code

--- tests/lesson-5/test3.spec.ts
+++ tests/lesson-5/test3.spec.ts
@@ -1,29 +1,33 @@
 import { test } from '@playwright/test';
 
 test('Todo page - Thêm 100 todo list', async ({ page }) => {
-    await page.goto('https://material.playwrightvn.com/');
-    await page.click("//a[contains(text(),'Bài học 3: Todo page')]");
-    const input = page.locator("//input[@id='new-task']");
-    const addButton = page.locator("//button[@id='add-task']");
-    for (let i = 1; i <= 100; i++) {
-        await input.fill(`Todo ${i}`);
-        await addButton.click();
-    }
+    await test.step('Add 100 todo items', async () => {
+        await page.goto('https://material.playwrightvn.com/');
+        await page.click("//a[contains(text(),'Bài học 3: Todo page')]");
+        const input = page.locator("//input[@id='new-task']");
+        const addButton = page.locator("//button[@id='add-task']");
+        for (let i = 1; i <= 100; i++) {
+            await input.fill(`Todo ${i}`);
+            await addButton.click();
+        }
+    });
 });
 
 // Xoa Todo le:
-test('Delete odd todos', async ({page}) => {
+test('Delete odd todos', async ({ page }) => {
     await page.goto('https://material.playwrightvn.com/');
     await page.click("//a[contains(text(),'Bài học 3: Todo page')]");
-    page.on('dialog', async (dialog) => {
-        await dialog.accept();
-    });
-    const deleteBtn = page.locator("//button[contains(text(),'Delete')]");
+    await test.step('Delete odd todo items', async () => {
+        page.on('dialog', async (dialog) => {
+            await dialog.accept();
+        });
+        const deleteBtn = page.locator("//button[contains(text(),'Delete')]");
 
-    for (let i = 0; i <= 100; i++) {
-        if (i % 2 !== 0) {
-            await deleteBtn.click();
+        for (let i = 0; i <= 100; i++) {
+            if (i % 2 !== 0) {
+                await deleteBtn.click();
+            }
         }
-    }
+    });
 });
 
